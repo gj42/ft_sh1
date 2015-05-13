d4ec82bd0a5633ed74dd07bdbe48382d80cda6c4
Guillaume JENSEN <gjensen@e2r4p16.42.fr>
2015-05-13 15:08:34 +0200

    ok

--- sh_builtin_env.c
+++ sh_builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: gjensen <gjensen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/12 16:43:20 by gjensen           #+#    #+#             */
-/*   Updated: 2015/05/09 18:25:23 by gjensen          ###   ########.fr       */
+/*   Updated: 2015/05/13 15:07:15 by gjensen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static void	sh_print_env(char **env)
 	}
 }
 
-static void	sh_builtin_env_exec(char **av, char **new_env, int i)
+static void	sh_builtin_env_exec(char **av, char ***new_env, int i)
 {
 	char	**array_path;
 	char	*new_cmd;
@@ -39,14 +39,14 @@ static void	sh_builtin_env_exec(char **av, char **new_env, int i)
 	if (av[i])
 	{
 		new_cmd = ft_joinarray(&av[i]);
-		array_path = sh_parse_env("PATH", new_env);
+		array_path = sh_parse_env("PATH", *new_env);
 		if (av[i])
-			sh_execute_cmd(new_cmd, &new_env, array_path);
+			sh_execute_cmd(new_cmd, new_env, array_path);
 		if (array_path)
 			ft_arrfree(&array_path);
 	}
 	else
-		sh_print_env(new_env);
+		sh_print_env(*new_env);
 }
 
 void		sh_builtin_env(char **av, char ***env)
@@ -73,6 +73,7 @@ void		sh_builtin_env(char **av, char ***env)
 		new_env = ft_arrcpy(*env);
 	if (av[i] || ft_strchr(av[i], '='))
 		sh_builtin_setenv(av, &new_env);
-	sh_builtin_env_exec(av, new_env, i);
-	ft_arrfree(&new_env);
+	sh_builtin_env_exec(av, &new_env, i);
+	if (new_env)
+		ft_arrfree(&new_env);
 }
